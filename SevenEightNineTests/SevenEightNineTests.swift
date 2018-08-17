89fb3b4d6268c27ca0a1f63b88a286d561308467
fitsyu <fitrah.syuhada@infiniqa.com>
2018-08-17 14:55:22 +0700

    Initial Commit

---
+++ SevenEightNineTests/SevenEightNineTests.swift
@@ -0,0 +1,36 @@
+//
+//  SevenEightNineTests.swift
+//  SevenEightNineTests
+//
+//  Created by Fitsyu on 17/08/2018.
+//  Copyright © 2018 o0o. All rights reserved.
+//
+
+import XCTest
+@testable import SevenEightNine
+
+class SevenEightNineTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
